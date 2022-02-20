f5995a12c8e96a245a4bca7872c1775b08ad27d1
Jeramy Sena <jeramy_sena@hotmail.com>
2022-02-20 17:11:30 -0600

    saving progress

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,25 +1,25 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-function Nav() {  
-    return (
-      <nav className="navbar">
+function Nav() {
+  return (
+    <nav className="navbar">
       <ul className="flex-row">
-      <li className="mx-2">
-        <Link to="/about">About Me</Link>
+        <li className="mx-2">
+          <Link to="/about">About Me</Link>
         </li>
         <li className="mx-2">
-        <Link to="/projects">Projects</Link>
+          <Link to="/projects">Projects</Link>
         </li>
         <li className="mx-2">
-        <Link to="/contact">Contact Me</Link>
+          <Link to="/contact">Contact Me</Link>
         </li>
         <li className="mx-2">
-        <Link to="/resume">Resume</Link>
+          <Link to="/resume">Resume</Link>
         </li>
       </ul>
     </nav>
-    );
-  }
-  
-  export default Nav;
+  );
+}
+
+export default Nav;
