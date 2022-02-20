f1874a7b85f23d99d08a854596c74208d86c8586
Jeramy Sena <jeramy_sena@hotmail.com>
2022-02-20 17:32:11 -0600

    saved progress

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -8,13 +8,12 @@ function About() {
       <img src={profilePic} className="profile-pic" style={{ width: "15%" }} alt="profile" />
       <div className="my-2">
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec bibendum turpis sed ex
-          condimentum molestie. Mauris condimentum lectus ut ornare dignissim. Mauris faucibus urna
-          mi, ac feugiat metus aliquam maximus. Proin aliquam justo nec diam vulputate vestibulum.
-          Aenean sollicitudin nulla at nisi ornare, nec suscipit massa eleifend. Morbi tristique
-          justo vel turpis sollicitudin, et tristique velit convallis. In hac habitasse platea
-          dictumst. Phasellus mattis nunc sed orci consequat laoreet. Praesent id nisl nibh.
-          Curabitur imperdiet ultricies mollis. In hac habitasse platea dictumst.
+          My name is Jeramy Sena. I graduated from Kansas Wesleyan University in 2003 with a Bachelor of Arts in Music.
+          I play multiple instruments including guitar, bass, drums, trumpet, and piano. I am looking for a new
+          career in coding and I am excited to see what the future brings to me in this new field. I have a lot
+          to learn but I have enjoyed the topics we have covered so far. In this web coding bootcamp
+          we have covered front end web application such as HTML, CSS, and Javascript. We also have
+          covered backend web application such as Express, Node.js, and React.
         </p>
       </div>
     </section>
